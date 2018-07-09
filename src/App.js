fad10c4000e3f8e0c860d560eb2db4cfb155da07
Florent Heyraud <florentheyraud.pro@gmail.com>
2018-07-09 14:28:30 +0200

    new-commit

--- src/App.js
+++ src/App.js
@@ -10,35 +10,48 @@ const key = 'f0fa246a92eacb18326af7323cf0d4f7'
 class App extends Component {
 
   state = {
-    input:'',
-    json:{}
+    input: '',
+    json: {},
+    error: '',
+    lockRequest: false
   }
+
 
 //active la fonction getDataApi avant l'affichage
 componentWillMount() {
     this.getDataApi();
   }
 
+
 //then capture la réponse et remplace le state json par ma reponse
 getDataApi = () => {
   axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${ this.state.input === '' ? 'Saigon' : this.state.input }&appid=${key}`)
   .then((response) => {
     console.log(response)
-  this.setState({ json: response }, () => {
+  this.setState({ json: response, error:'' }, () => {
     
   })
   })
 
   .catch((error) => {
+    this.setState({ error: 'ça existe pas' }, () => {
+    
+    })
     console.log(error);
   } )
 }
 
-//récupère la saisie de l'input
+
 inputStock = (event) => {
   this.setState({input: event.target.value}, () => {
-    console.log(this.state.input)
-  })
+    if(!this.state.lockRequest){
+      this.setState({lockRequest: true})
+    setTimeout(() => {
+      this.clickSearch()
+      this.setState({lockRequest: false})
+    },2000)
+  }
+ })
 }
 
 
@@ -60,6 +73,7 @@ clickSearch = () => {
       <div className="App">
         <Search search={ this.inputStock } click={this.clickSearch } />
         { renderInfo() }
+        <p>{ this.state.error }</p>
       </div>
     );
   }
